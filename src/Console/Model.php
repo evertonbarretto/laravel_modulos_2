a1593043b25dee869384193a53f34091e81d9a71
Everton Barretto <evertonbarreto@aformulabr.com.br>
2021-09-10 14:25:32 -0300

    models

---
+++ src/Console/Model.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace evertonbarretto\laravel_modulos_2\Console;
+
+use Illuminate\Console\Command;
+
+class Model extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'modulo:make-model {modulo} {entidade}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $path = "modules//{$this->argument('modulo')}//Models//{$this->argument('entidade')}.php";
+        $this->createFile($path,$this->argument('modulo'),$this->argument('entidade'));
+        return 0;
+    }
+
+    private function createFile($path,$modulo,$entidade)
+    {
+
+        $factory = "\n  protected static function newFactory()\n{\n     return \Modules\Cadastro\database\factories\CategoriaFactory::new();\n  }";
+        $factory = "";
+        $contents =
+            "<?php\n\nnamespace Modulos\\".$modulo."\\Models;\n\nuse Illuminate\Database\Eloquent\Model;\nuse Illuminate\Database\Eloquent\Factories\HasFactory;\n\nclass ".$entidade." extends Model\n{\n    use HasFactory;".$factory."\n}";
+
+
+        $myfile = fopen($path, "w") or die("Unable to open file!");
+        $txt = $contents;
+        fwrite($myfile, $txt);
+        fclose($myfile);
+
+
+
+    }
+}
